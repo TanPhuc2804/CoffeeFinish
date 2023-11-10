75f10cbba14cc2facef705629bb90cd05f9f69a4
TanPhuc2804 <phantanphuc282004@gmail.com>
2023-11-10 20:05:17 +0700

    Finish

---
+++ Coffee_web/Coffee_web/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Coffee_web.Models;
+
+namespace CoffeeManager.Controllers
+{
+    public class CustomersController : Controller
+    {
+        CoffeeEntities db = new CoffeeEntities();
+        // GET: Customers
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Customer cus)
+        {
+            try
+            {
+
+                db.Customers.Add(cus);
+                db.SaveChanges();
+                return RedirectToAction("Show", "ShoppingCart");
+            }catch (Exception ex)
+            {
+                RedirectToAction("Show", "ShoppingCart");
+            }
+
+            return View();
+        }
+    }
+}
